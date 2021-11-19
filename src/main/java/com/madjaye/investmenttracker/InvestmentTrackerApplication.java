af0d77f753f3acffc06bbbe5f4a87e8aed9690c9
madjaye <adjaye.m@gmail.com>
2021-11-19 17:00:47 +0000

    Adds ability to containerise app using docker + ability to spin up postgres container via docker image

--- src/main/java/com/madjaye/investmenttracker/InvestmentTrackerApplication.java
+++ src/main/java/com/madjaye/investmenttracker/InvestmentTrackerApplication.java
@@ -2,12 +2,20 @@ package com.madjaye.investmenttracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class InvestmentTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InvestmentTrackerApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Investment Tracking World";
 	}
 
 }
