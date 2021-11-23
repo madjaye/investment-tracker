13df119e51a07d8eb25861635c73e6fd93b2822f
madjaye <adjaye.m@gmail.com>
2021-11-23 16:43:08 +0000

    Integrate tests with testcontainers for postgres

--- src/test/java/com/madjaye/investmenttracker/InvestmentTrackerApplicationTests.java
+++ src/test/java/com/madjaye/investmenttracker/InvestmentTrackerApplicationTests.java
@@ -1,13 +1,27 @@
 package com.madjaye.investmenttracker;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
 class InvestmentTrackerApplicationTests {
 
+	@Container
+	private final PostgreSQLContainer<?> postgresqlContainer = new PostgreSQLContainer<>("postgres:14.0");
+
 	@Test
-	void contextLoads() {
+	void test() {
+		assertThat(postgresqlContainer.isRunning()).isTrue();
 	}
 
 }
