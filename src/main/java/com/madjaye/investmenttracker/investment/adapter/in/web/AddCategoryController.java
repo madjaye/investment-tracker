51ac277d4a30130b540d2020ac601a062107bcd4
madjaye <adjaye.m@gmail.com>
2021-11-26 15:59:01 +0000

    Handle duplicate categories in the controller layer by returning conflict http status code

--- src/main/java/com/madjaye/investmenttracker/investment/adapter/in/web/AddCategoryController.java
+++ src/main/java/com/madjaye/investmenttracker/investment/adapter/in/web/AddCategoryController.java
@@ -2,6 +2,7 @@ package com.madjaye.investmenttracker.investment.adapter.in.web;
 
 import com.madjaye.investmenttracker.investment.application.port.in.AddCategoryCommand;
 import com.madjaye.investmenttracker.investment.application.port.in.AddCategoryUseCase;
+import com.madjaye.investmenttracker.investment.application.service.BusinessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +15,14 @@ record AddCategoryController(AddCategoryUseCase addCategoryUseCase) {
     @PostMapping(path = "/category/{category}/user/{userId}")
     ResponseEntity<Object> addCategory(@PathVariable("category") String category, @PathVariable("userId") Long userId) {
         var addCategoryCommand = new AddCategoryCommand(category, userId);
-        addCategoryUseCase.addCategory(addCategoryCommand);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        HttpStatus httpStatus;
+        try {
+            addCategoryUseCase.addCategory(addCategoryCommand);
+            httpStatus = HttpStatus.CREATED;
+        } catch (BusinessException e) {
+            httpStatus = HttpStatus.CONFLICT;
+        }
+        return ResponseEntity.status(httpStatus).build();
     }
 
 }
--- src/test/java/com/madjaye/investmenttracker/investment/adapter/in/web/AddCategoryControllerTest.java
+++ src/test/java/com/madjaye/investmenttracker/investment/adapter/in/web/AddCategoryControllerTest.java
@@ -2,6 +2,7 @@ package com.madjaye.investmenttracker.investment.adapter.in.web;
 
 import com.madjaye.investmenttracker.investment.application.port.in.AddCategoryCommand;
 import com.madjaye.investmenttracker.investment.application.port.in.AddCategoryUseCase;
+import com.madjaye.investmenttracker.investment.application.service.BusinessException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.willThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,7 +24,7 @@ class AddCategoryControllerTest {
     private AddCategoryUseCase addCategoryUseCase;
 
     @Test
-    void shouldAddCategory() throws Exception {
+    void shouldReturnCreatedAfterAddingCategory() throws Exception {
         // Given
         var name = "the category";
         var userId = 1L;
@@ -36,6 +38,25 @@ class AddCategoryControllerTest {
 
         // Then
         then(addCategoryUseCase).should().addCategory(addCategoryCommand);
+    }
+
+    @Test
+    void shouldReturnConflictIfBusinessExceptionThrown() throws Exception {
+        // Given
+        var name = "the duplicate category";
+        var userId = 1L;
+        var addCategoryCommand = new AddCategoryCommand(name, userId);
+
+        // When
+        willThrow(BusinessException.class)
+            .given(addCategoryUseCase).addCategory(addCategoryCommand);
+
+        mockMvc.perform(post("/category/{category}/user/{userId}",
+            name, userId)
+            .header("Content-Type", "application/json"))
+
+            // Then
+            .andExpect(status().isConflict());
     }
 
 }
