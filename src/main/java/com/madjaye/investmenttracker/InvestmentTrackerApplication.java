7a59b03884d9aa800e7f6b97278d93a996eef5f2
madjaye <adjaye.m@gmail.com>
2021-11-26 11:35:08 +0000

    Add Spring Doc Swagger dependency for API documentation

--- src/main/java/com/madjaye/investmenttracker/InvestmentTrackerApplication.java
+++ src/main/java/com/madjaye/investmenttracker/InvestmentTrackerApplication.java
@@ -1,28 +1,16 @@
 package com.madjaye.investmenttracker;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableTransactionManagement
-@RestController
 public class InvestmentTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InvestmentTrackerApplication.class, args);
 	}
 
-	@Value("${spring.datasource.url}")
-	private String url;
-
-	@RequestMapping("/")
-	public String home() {
-		return "Hello Investment Tracking World " + url;
-	}
 
 }
