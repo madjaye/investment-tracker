9d170ab2aa1db73cf339d1018a70c9b626cf85ca
madjaye <adjaye.m@gmail.com>
2021-12-07 15:01:54 +0000

    Convert AddCategoryService from Record to Class so as not to unecessarily expose a getter to the SaveCategoryPort

--- src/main/java/com/madjaye/investmenttracker/investment/application/service/AddCategoryService.java
+++ src/main/java/com/madjaye/investmenttracker/investment/application/service/AddCategoryService.java
@@ -4,11 +4,15 @@ import com.madjaye.investmenttracker.investment.application.port.in.AddCategoryC
 import com.madjaye.investmenttracker.investment.application.port.in.AddCategoryUseCase;
 import com.madjaye.investmenttracker.investment.application.port.out.SaveCategoryPort;
 import com.madjaye.investmenttracker.investment.domain.Category;
+import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
-public record AddCategoryService(SaveCategoryPort saveCategoryPort) implements AddCategoryUseCase {
+@RequiredArgsConstructor
+public class AddCategoryService implements AddCategoryUseCase {
+
+    private final SaveCategoryPort saveCategoryPort;
 
     @Override
     public void addCategory(AddCategoryCommand addCategoryCommand) {
