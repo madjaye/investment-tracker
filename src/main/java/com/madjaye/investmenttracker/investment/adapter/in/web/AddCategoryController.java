2e34d8abe36d1fed6cb3e310f1a695524899102d
madjaye <adjaye.m@gmail.com>
2021-12-07 15:04:56 +0000

    Convert AddCategoryController from Record to Class so as not to unecessarily expose a getter to the AddCategoryUseCase

--- src/main/java/com/madjaye/investmenttracker/investment/adapter/in/web/AddCategoryController.java
+++ src/main/java/com/madjaye/investmenttracker/investment/adapter/in/web/AddCategoryController.java
@@ -3,6 +3,7 @@ package com.madjaye.investmenttracker.investment.adapter.in.web;
 import com.madjaye.investmenttracker.investment.application.port.in.AddCategoryCommand;
 import com.madjaye.investmenttracker.investment.application.port.in.AddCategoryUseCase;
 import com.madjaye.investmenttracker.investment.application.service.BusinessException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-record AddCategoryController(AddCategoryUseCase addCategoryUseCase) {
+@RequiredArgsConstructor
+class AddCategoryController {
+
+    private final AddCategoryUseCase addCategoryUseCase;
 
     @PostMapping(path = "/category/{category}/user/{userId}")
     ResponseEntity<Object> addCategory(@PathVariable("category") String category, @PathVariable("userId") Long userId) {
